73ba4092627c6a1d6ca77f3f718abeb3f03ec856
sebbod <sebbod@laposte.net>
2022-01-11 18:15:30 +0100

    add remember_me and service notification

---
+++ src/Notification/Sender.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Notification;
+
+use App\Entity\User;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class Sender
+{
+
+    protected $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function sendNewUserNotificationToAdmin(UserInterface $user):void
+    {
+        // save mail to file \public\debug.txt
+        //file_put_contents('debug.txt', $user->getEmail());
+
+        $message = new Email();
+        $message->from('acounts@series.com')
+            ->to('admin@series.com')
+            ->subject('new account created on series.com!')
+            ->html('<h1>New account!</h1>email: '.$user->getEmail());
+
+        $this->mailer->send($message);
+    }
+}
